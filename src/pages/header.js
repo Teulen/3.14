e9351ac11b3bf3e10a4233f68b68a5e1027a25aa
Temuulen Saikhanbayar <temuulensaikhanbayar31@gmail.com>
2024-03-22 01:16:59 +0800

    First commit

--- src/pages/header.js
+++ src/pages/header.js
@@ -7,7 +7,7 @@ import Link from 'next/link';
 export default function Header() {
     const router = useRouter();
   return (
-    <div className='w-full h-20 bg-slate-900 items-center flex justify-center relative shrink'>
+    <div className='w-full h-20 bg-slate-600 dark:bg-slate-900 items-center flex justify-center relative shrink'>
         <div className='max-sm:w-full sm:w-full md:w-full lg:w-[1000px] xl:w-[1100px] h-full shrink-0 flex items-center relative'>
             <a className='flex w-[16%] h-12 items-center left-0 absolute'>
                 <img src={'https://merchant.monpay.mn/stat/images/candy-logo.png'} className='h-full w-12'/>
@@ -15,24 +15,24 @@ export default function Header() {
             </a>
 
             <div className='w-[70%] lg:w-[80%] xl:w-[70%] h-full justify-between flex items-center max-sm:hidden sm:invisible md:invisible lg:visible xl:visible 2xl:visible absolute right-0'>
-                        <a className='w-[12%] h-full flex items-center cursor-pointer justify-center border-b-[3px] border-lime-500 border-opacity-0 hover:border-opacity-100'>
-                            <h1 className='text-black dark:text-white text-sm font-light'>Нүүр хуудас</h1>
+                        <a className='w-[12%] h-full flex items-center cursor-pointer justify-center hover:decoration-indigo-500 hover:underline '>
+                            <h1 className='text-slate-300 dark:text-white text-sm font-light '>Нүүр хуудас</h1>
                         </a>
-                        <a className='w-[15%] h-full flex items-center cursor-pointer justify-center border-b-[3px] border-lime-500 border-opacity-0 hover:border-opacity-100'>
-                            <h1 className='text-black dark:text-white text-sm font-light'>Монпэйн тухай</h1>
+                        <a className='w-[15%] h-full flex items-center cursor-pointer justify-center hover:decoration-indigo-500 hover:underline '>
+                            <h1 className='text-slate-300 dark:text-white text-sm font-light'>Монпэйн тухай</h1>
                         </a>
-                        <a className='w-[12%] h-full flex items-center cursor-pointer justify-center border-b-[3px] border-lime-500 border-opacity-0 hover:border-opacity-100'>
-                            <h1 className='text-black dark:text-white text-sm font-light'>Тусламж</h1>
+                        <a className='w-[12%] h-full flex items-center cursor-pointer justify-center hover:decoration-indigo-500 hover:underline '>
+                            <h1 className='text-slate-300 dark:text-white text-sm font-light'>Тусламж</h1>
                         </a>
-                        <a className='w-[14%] h-full flex items-center cursor-pointer justify-center border-b-[3px] border-lime-500 border-opacity-0 hover:border-opacity-100'>
-                            <h1 className='text-black dark:text-white text-sm font-light'>Онлайн бүртгэл</h1>
+                        <a className='w-[14%] h-full flex items-center cursor-pointer justify-center hover:decoration-indigo-500 hover:underline '>
+                            <h1 className='text-slate-300 dark:text-white text-sm font-light'>Онлайн бүртгэл</h1>
                         </a>
-                        <button className='py-2 px-6 bg-gradient-to-r from-purple-500 to-indigo-500 rounded-lg'>Нэвтрэх</button>
-                        <a className='w-[12%] h-full flex items-center cursor-pointer justify-center border-b-[3px] border-lime-500 border-opacity-0 hover:border-opacity-100'>
-                            <h1 className='text-black dark:text-white text-sm font-light'>OPEN API</h1>
+                        <button className='py-2 px-6 bg-gradient-to-r from-purple-500 to-indigo-500 rounded-lg text-slate-300 dark:text-white'>Нэвтрэх</button>
+                        <a className='w-[12%] h-full flex items-center cursor-pointer justify-center hover:decoration-indigo-500 hover:underline '>
+                            <h1 className='text-slate-300 dark:text-white text-sm font-light'>OPEN API</h1>
                         </a>
-                        <a className='w-[16%] h-full flex items-center cursor-pointer justify-center border-b-[3px] border-lime-500 border-opacity-0 hover:border-opacity-100'>
-                            <h1 className='text-black dark:text-white text-sm font-light'>МОНПЭЙ МАРКЕТ</h1>
+                        <a className='w-[16%] h-full flex items-center cursor-pointer justify-center hover:decoration-indigo-500 hover:underline '>
+                            <h1 className='text-slate-300 dark:text-white text-sm font-light'>МОНПЭЙ МАРКЕТ</h1>
                         </a>
                         <button className='py-2 px-6 bg-gradient-to-r from-purple-500 to-indigo-500 rounded-lg max-sm:visible sm:visible md:visible lg:hidden xl:hidden mr-10'>Нэвтрэх</button>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,21 +6,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className='w-full h-full flex flex-col overflow-auto bg-white dark:bg-slate-900'>
+    <div className='w-full h-full flex flex-col overflow-auto bg-slate-600 dark:bg-slate-900'>
       <Header/>
 
 
       {/* Text-1 */}
         <div className="w-full h-1/2">
             <div className="w-full h-full flex flex-col items-center justify-center">
-                  <h1 className="text-2xl font-normal text-slate-100 dark:text-slate-300 mt-10 mb-2">
-                    <span className="text-slate-100 dark:text-slate-300">МОНПЭЙ МАРКЕТ</span>
+                  <h1 className="text-2xl font-normal text-slate-400 dark:text-slate-300 mt-10 mb-2">
+                    <span className="text-slate-400 dark:text-slate-300">МОНПЭЙ МАРКЕТ</span>
                   </h1>
                   <div className="bg-white h-0.5 w-20 rounded mb-10"></div>
-                  <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">
-                    <span className="text-slate-100 dark:text-slate-300">Худалдаа үйлчилгээ эрхлэгч та Монпэй мерчантын энэхүү платформоор дамжуулж өөрийн онлайн вэб дэлгүүртэй болох боломжтой.</span>
+                  <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">
+                    <span className="text-slate-400 dark:text-slate-300">Худалдаа үйлчилгээ эрхлэгч та Монпэй мерчантын энэхүү платформоор дамжуулж өөрийн онлайн вэб дэлгүүртэй болох боломжтой.</span>
                   </h2>
-                  <h2 className="text-md font-light text-slate-100 dark:text-slate-300 mb-5">Та МОНПЭЙ МАРКЕТ платформоор дамжуулж:</h2>
+                  <h2 className="text-md font-light text-slate-400 dark:text-slate-300 mb-5">Та МОНПЭЙ МАРКЕТ платформоор дамжуулж:</h2>
             </div>
         </div>
 
@@ -28,47 +28,47 @@ export default function Home() {
         {/* Icons */}
 
         <div className="w-full h-1/2 flex justify-center">
-          <div className="grid w-8/12 h-full xl:grid-cols-6 ">
+          <div className="grid w-8/12 h-full xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-3 max-sm:grid-cols-3">
 
             <a className="flex flex-col items-center justify-center">
               <img src="https://merchant.monpay.mn/stat/ministore/1.png"/>
-              <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">Дэлгүүр нээх</h2>
+              <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">Дэлгүүр нээх</h2>
             </a>
             <a className="flex flex-col items-center justify-center">
               <img src="https://merchant.monpay.mn/stat/ministore/2.png"/>
-              <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">Бараа оруулах</h2>
+              <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">Бараа оруулах</h2>
             </a>
             <a className="flex flex-col items-center justify-center">
               <img src="https://merchant.monpay.mn/stat/ministore/3.png"/>
-              <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">Бараа удирдах</h2>
+              <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">Бараа удирдах</h2>
             </a>
             <a className="flex flex-col items-center justify-center">
               <img src="https://merchant.monpay.mn/stat/ministore/4.png"/>
-              <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">Барааны үлдэгдэл удирдах</h2>
+              <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">Барааны үлдэгдэл удирдах</h2>
             </a>
             <a className="flex flex-col items-center justify-center">
               <img src="https://merchant.monpay.mn/stat/ministore/5.png"/>
-              <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">Ангилал удирдах</h2>
+              <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">Ангилал удирдах</h2>
             </a>
             <a className="flex flex-col items-center justify-center">
               <img src="https://merchant.monpay.mn/stat/ministore/6.png"/>
-              <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">Сонголт оруулах</h2>
+              <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">Сонголт оруулах</h2>
             </a>
             <a className="flex flex-col items-center justify-center">
               <img src="https://merchant.monpay.mn/stat/ministore/7.png"/>
-              <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">Бүтээгдэхүүн онцлох</h2>
+              <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">Бүтээгдэхүүн онцлох</h2>
             </a>
             <a className="flex flex-col items-center justify-center">
               <img src="https://merchant.monpay.mn/stat/ministore/8.png"/>
-              <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">Захиалга шалгах</h2>
+              <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">Захиалга шалгах</h2>
             </a>
             <a className="flex flex-col items-center justify-center">
               <img src="https://merchant.monpay.mn/stat/ministore/9.png"/>
-              <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">Захиалга оруулах</h2>
+              <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">Захиалга оруулах</h2>
             </a>
             <a className="flex flex-col items-center justify-center">
               <img src="https://merchant.monpay.mn/stat/ministore/10.png"/>
-              <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">Захиалгын төлөв өөрчлөх</h2>
+              <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">Захиалгын төлөв өөрчлөх</h2>
             </a>
 
           </div>
@@ -78,11 +78,11 @@ export default function Home() {
         {/* Text-2 */}
         <div className="w-full h-1/2">
             <div className="w-full h-full flex flex-col items-center justify-center">
-                  <h1 className="text-lg font-normal text-slate-100 dark:text-slate-300 my-10">
-                    <span className="text-slate-100 dark:text-slate-300">Та үүсгэсэн дэлгүүрээ Монпэй апликейшнд байршуулж, Монпэй апликейшны 400,000+ хэрэглэгчдэд бүтээгдэхүүнээ шууд хүргээрэй.</span>
+                  <h1 className="text-lg font-normal text-slate-400 dark:text-slate-300 my-10">
+                    <span className="text-slate-400 dark:text-slate-300 text-center">Та үүсгэсэн дэлгүүрээ Монпэй апликейшнд байршуулж, Монпэй апликейшны 400,000+ хэрэглэгчдэд бүтээгдэхүүнээ шууд хүргээрэй.</span>
                   </h1>
-                  <h2 className="text-center text-md font-light text-slate-100 dark:text-slate-300">
-                    <span className="text-slate-100 dark:text-slate-300">Дэлгүүрээ үүсгэхийн тулд та Монпэй мерчант болсон байх шаардлагатай ба яг одоо <a className="cursor-pointer font-bold">ЭНД ДАРЖ</a> хүсэлтээ илгээнэ үү.</span>
+                  <h2 className="text-center text-md font-light text-slate-400 dark:text-slate-300">
+                    <span className="text-slate-400 dark:text-slate-300">Дэлгүүрээ үүсгэхийн тулд та Монпэй мерчант болсон байх шаардлагатай ба яг одоо <a className="cursor-pointer font-bold">ЭНД ДАРЖ</a> хүсэлтээ илгээнэ үү.</span>
                   </h2>
             </div>
         </div>
@@ -96,38 +96,38 @@ export default function Home() {
 
         {/* Contact */}
         <div className="w-full h-1/4 flex justify-center mt-40">
-          <div className="w-8/12 h-full grid xl:grid-cols-4">
+          <div className="w-8/12 h-full grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 max-sm:grid-cols-1 gap-10">
 
             <div>
               <div className="flex w-full gap-4">
                 <img src="https://merchant.monpay.mn/stat/images/icon-headset.png" className="w-12 h-12"/>
-                <h1 className="text-wrap">Харилцагчийн мэдээллийн төв:</h1>
+                <h1 className="text-wrap text-slate-400 dark:text-slate-300">Харилцагчийн мэдээллийн төв:</h1>
               </div>
-              <h1 className="ml-10 text-xl">1800-1199</h1>
+              <h1 className="ml-10 text-xl text-slate-400 dark:text-slate-300">1800-1199</h1>
             </div>
 
             <div>
               <div className="flex w-full gap-4">
                 <img src="https://merchant.monpay.mn/stat/images/icon-email.png" className="w-12 h-12"/>
-                <h1 className="text-wrap">И-мэйл хаяг:</h1>
+                <h1 className="text-wrap text-slate-400 dark:text-slate-300">И-мэйл хаяг:</h1>
               </div>
-              <h1 className="ml-16 text-md font-light">digitalbusiness@mobicom.mn</h1>
+              <h1 className="ml-16 text-md font-light text-slate-400 dark:text-slate-300">digitalbusiness@mobicom.mn</h1>
             </div>
 
             <div>
               <div className="flex w-full gap-4">
                 <img src="https://merchant.monpay.mn/stat/images/icon-phone-contact.png" className="w-12 h-12"/>
-                <h1 className="text-wrap">Холбогдох утас:</h1>
+                <h1 className="text-slate-400 dark:text-slate-300 text-wrap">Холбогдох утас:</h1>
               </div>
-              <h1 className="ml-10 text-xl">75752255</h1>
+              <h1 className="text-slate-400 dark:text-slate-300 ml-10 text-xl">75752255</h1>
             </div>
 
             <div>
               <div className="flex w-full gap-4">
                 <img src="https://merchant.monpay.mn/stat/images/icon-headset.png" className="w-12 h-12"/>
-                <h1 className="text-wrap">Манай хаяг:</h1>
+                <h1 className=" text-slate-400 dark:text-slate-300 text-wrap">Манай хаяг:</h1>
               </div>
-              <h1 className="ml-16 text-md font-light text-wrap">УБ, СБД, 1-р хороо, Юнескогийн гудамж, MПМ цогцолбор3 үгт хаяг: Сүлжмэл, хууль, нууцлах</h1>
+              <h1 className="text-slate-400 dark:text-slate-300 ml-16 text-md font-light text-wrap">УБ, СБД, 1-р хороо, Юнескогийн гудамж, MПМ цогцолбор3 үгт хаяг: Сүлжмэл, хууль, нууцлах</h1>
             </div>
 
           </div>
